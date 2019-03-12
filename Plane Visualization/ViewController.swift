40a1f7ab1148351b1102926969da52ffc338efa6
Ricky Munz <rmunz@captechventures.com>
2019-03-12 12:38:29 -0400

    Add view lifecycle methods.

--- Plane Visualization/ViewController.swift
+++ Plane Visualization/ViewController.swift
@@ -10,35 +10,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
+
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal]
         sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
     }
 
     // MARK: - ARSCNViewDelegate
