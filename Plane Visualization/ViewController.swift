a8f531a1bd3296c761e9fdedb387204249d790f4
Ricky Munz <rmunz@captechventures.com>
2019-03-12 20:47:07 -0400

    Add SCNPlane for ARPlaneAnchor.

--- Plane Visualization/ViewController.swift
+++ Plane Visualization/ViewController.swift
@@ -24,7 +24,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        
+        // Cast ARAnchor to ARPlaneAnchor
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+
+        // Create SCNGeometry from ARPlaneAnchor details
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let planeGeometry = SCNPlane(width: width, height: height)
+
+        // Create a SCNNode from the SCNPlane geometry
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+
+        // Add the newly created plane node as a child of the node created for the ARAnchor
+        node.addChildNode(planeNode)
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
